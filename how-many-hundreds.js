de82cafbfab6cb4a34bf02aaa8d1c80d255773ce
vagrant <anthonykao10@gmail.com>
2019-07-26 06:17:46 +0000

    Add how-many-hundreds.js

---
+++ how-many-hundreds.js
@@ -0,0 +1,14 @@
+/**
+ * Determines how many hundreds can be made from a number.
+ * @param {number} num 
+ * @return {number}
+ */
+function howManyHundreds(num) {
+  return (num - (num % 100)) / 100;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
